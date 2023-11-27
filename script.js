7f958717637eacb915a7da9c755167b77caf0716
mshecyan <43683504+kotoshow@users.noreply.github.com>
2023-11-27 17:13:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let item_add_btn = document.getElementById("create");
 //функция отметки выполнения
 function SuccessCheck() {
     this.firstChild.classList.toggle("check");
-    this.nextSibling.classList.toggle("task-done");
+    this.parentElement.classList.toggle("task-done");
 }
 
 //функция редактирования элемента
@@ -34,15 +34,20 @@ function ItemDelete() {
 }
 
 //функция вставки нового элемента в список
-function ItemAdd(txt) {
-    item_add_btn.insertAdjacentHTML("beforebegin", 
-        "<div class=\"item\">" +
-            "<div class=\"item-box\">" + 
-                "<div class=\"item-icon success-box\">" + 
-                    "<div class=\"non-check\"></div>" + 
-                "</div>" +
-                "<input type=\"text\" placeholder=\"Начните писать...\" class=\"item-text\">" +
-            "</div>" +
+function ItemAdd(txt, status, edit) {
+    let item_object = "<div class=\"item\">" +
+        "<div class=\"item-box\">" + 
+            "<div class=\"item-icon success-box\">" + 
+                "<div class=\"non-check\"></div>" + 
+            "</div>";
+    
+    if (edit) {
+        item_object += "<input type=\"text\" placeholder=\"Начните писать...\" class=\"item-text\">";
+    }
+    else {
+        item_object += "<div class=\"item-text\">" + txt + "</div>";
+    }
+    item_object += "</div>" +
             "<div class=\"item-control\">" +
                 "<div class=\"item-icon\">" +
                     "<div class=\"edit\"></div>" +
@@ -51,10 +56,16 @@ function ItemAdd(txt) {
                     "<div class=\"delete\"></div>" +
                 "</div>" +
             "</div>" +
-        "</div>");
+        "</div>";
+
+    item_add_btn.insertAdjacentHTML("beforebegin", item_object);
 
     current_elem = document.getElementsByClassName("success-box");
     current_elem[current_elem.length-1].addEventListener("click", SuccessCheck);
+    if (status) {
+        current_elem[current_elem.length-1].firstChild.classList.add("check");
+        current_elem[current_elem.length-1].parentElement.classList.add("task-done");
+    }
 
     current_edit = document.getElementsByClassName("edit");
     current_edit[current_edit.length-1].parentElement.addEventListener("click", ItemEdit);
@@ -62,17 +73,17 @@ function ItemAdd(txt) {
     current_delete = document.getElementsByClassName("delete");
     current_delete[current_delete.length-1].parentElement.addEventListener("click", ItemDelete);
 
-    current_text = document.getElementsByClassName("item-text");
-    current_text[current_text.length-1].value = txt;
-    current_text[current_text.length-1].addEventListener("focusout", ItemEndEdit);
-    current_text[current_text.length-1].focus();
+    if (edit) {
+        current_text = document.getElementsByClassName("item-text");
+        current_text[current_text.length-1].addEventListener("focusout", ItemEndEdit);
+        current_text[current_text.length-1].focus();
+    }
 }
 
 //обработка события нажатия на кнопку "Добавить"
 function ItemAddEvent() {
-    ItemAdd("");
+    ItemAdd("", false, true);
 }
-
 
 //нажатие на кнопку "Добавить"
 item_add_btn.addEventListener("click", ItemAddEvent);
@@ -80,21 +91,28 @@ item_add_btn.addEventListener("click", ItemAddEvent);
 //обработка события закрытия страницы
 window.onunload = function() {
     let items = document.getElementsByClassName("item-text");
+    let check = document.getElementsByClassName("non-check");
     let item_txt = Array();
+    let check_box = Array();
 
     for (let i = 0; i < items.length; i++) {
+        items[i].blur();
         item_txt.push(items[i].textContent);
+        check_box.push(check[i].classList.contains("check"));
     }
 
     localStorage.setItem("todo_items", JSON.stringify(item_txt));
+    localStorage.setItem("todo_items_check", JSON.stringify(check_box));
 }
 
 //обработка события загрузки страницы
 window.onload = function() {
     let items = localStorage.getItem("todo_items");
+    let check_box = localStorage.getItem("todo_items_check");
     item_txt = JSON.parse(items);
+    check = JSON.parse(check_box);
 
     for (let i = 0; i < item_txt.length; i++) {
-        ItemAdd(item_txt[i]);
+        ItemAdd(item_txt[i], check[i], false);
     }
 }
